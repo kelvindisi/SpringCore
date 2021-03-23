2ad0df8045e188ea1461101474e0fff9753e27f0
Kelvin Ndisi <kelvindisi@yahoo.com>
2021-03-23 18:25:43 +0300

    autowiring byType

--- src/main/java/org/devkiu/spring/autowiring/byTypeXML/Main.java
+++ src/main/java/org/devkiu/spring/autowiring/byTypeXML/Main.java
@@ -1,11 +1,11 @@
-package org.devkiu.spring.autowiring.byNameXML;
+package org.devkiu.spring.autowiring.byTypeXML;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("autowiring/byNameAutowiring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("autowiring/byTypeAutowiring.xml");
         System.out.println(context.getBean("teacher", Teacher.class));
     }
 }
---
+++ src/main/java/org/devkiu/spring/autowiring/byTypeXML/Teacher.java
@@ -0,0 +1,22 @@
+package org.devkiu.spring.autowiring.byTypeXML;
+
+public class Teacher {
+    private String fullName;
+    private Address address;
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "fullName='" + fullName + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
